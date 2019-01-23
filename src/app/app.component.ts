cd49041ec86935d617bb1d37ba56a58168f76513
leoreyes84 <leoreyes84@gmail.com>
2019-01-23 09:52:31 -0500

    version final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'Leonardo';
+  nombre2 = 'LeonArdo reYEs riverO';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+
+  setTimeout(() => resolve('Llego la data!'), 3500);
+
+  });
+
+  fecha = new Date();
+
+  video = '2Vv-BfVoq4g';
+
+  activar = true;
+
 }
